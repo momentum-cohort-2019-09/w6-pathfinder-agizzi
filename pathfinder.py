772d95a41f48371647011d2afca5b8c083f3d774
agizzi <alexandra.n.gizzi@gmail.com>
2019-10-17 17:38:31 -0400

    elevation lists and color lists

---
+++ pathfinder.py
@@ -0,0 +1,35 @@
+import math
+
+with open("elevation_small.txt") as text_file:
+    text_contents = text_file.read()
+
+elevations = [[int(each) for each in line.split()]
+              for line in text_contents.split("\n")]
+# print(elevations[0])
+
+# print(len(elevations[0]))
+
+min = elevations[0][0]
+max = elevations[0][0]
+
+for each in elevations:
+    for integer in each:
+        if integer < min:
+            min = integer
+        if integer > max:
+            max = integer
+# print(min)
+# print(max)
+
+colors_big_list = []
+little_rows_of_colors = []
+
+for rows in elevations:
+    for number in rows:
+        color_int = round(((number - min) / (max-min)) * 255)
+        little_rows_of_colors.append(color_int)
+    colors_big_list.append(little_rows_of_colors)
+    little_rows_of_colors = []
+
+# print(colors_big_list[0])
+# print(len(colors_big_list[0]))
