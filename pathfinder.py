886078f842f816cc9339ce594314c54ec138c85d
agizzi <alexandra.n.gizzi@gmail.com>
2019-10-18 14:32:14 -0400

    map created with OOP :)

--- pathfinder.py
+++ pathfinder.py
@@ -1,44 +1,58 @@
 import math
 from PIL import Image
+import numpy as np
 
 
 class Map:
     def __init__(self, file):
         self.file = file
+        self.elevations = []
+        self.min_elevation = []
+        self.max_elevation = []
+        self.text_contents = []
+        self.colors_big_list = []
+        self.little_rows_of_colors = []
 
-    def read_file(self, file):
-        with open(file) as text_file:
-            text_contents = text_file.read()
+    def read_file(self):
+        with open(self.file) as text_file:
+            self.text_contents = text_file.read()
 
-        elevations = [[int(each) for each in line.split()]
-                      for line in text_contents.split("\n")]
+    def find_elevations(self):
+        self.elevations = [[int(each) for each in line.split()]
+                           for line in self.text_contents.split("\n")]
 
-        min = elevations[0][0]
-        max = elevations[0][0]
-        print("hi")
+    def find_min_and_max(self):
+        self.min_elevation = self.elevations[0][0]
+        self.max_elevation = self.elevations[0][0]
 
-        for each in elevations:
+        for each in self.elevations:
             for integer in each:
-                if integer < min:
-                    min = integer
-                if integer > max:
-                    max = integer
+                if integer < self.min_elevation:
+                    self.min_elevation = integer
+                if integer > self.max_elevation:
+                    self.max_elevation = integer
 
-        colors_big_list = []
-        little_rows_of_colors = []
-
-        for rows in elevations:
+    def get_colors_from_elevations(self):
+        for rows in self.elevations:
             for number in rows:
-                color_int = round(((number - min) / (max-min)) * 255)
-                little_rows_of_colors.append(color_int)
-        colors_big_list.append(little_rows_of_colors)
-        little_rows_of_colors = []
+                color_int = round(
+                    ((number - self.min_elevation) / (self.max_elevation-self.min_elevation)) * 255)
+                self.little_rows_of_colors.append(color_int)
+            self.colors_big_list.append(self.little_rows_of_colors)
+            self.little_rows_of_colors = []
         print("i am running")
+        # print(self.colors_big_list)
 
     def create_map_image(self):
-        image = Image.new('RGB', (600, 600), 'black')
-        image.save('pathfinder.png')
+        print("create_map")
+        img = Image.fromarray(np.uint8(self.colors_big_list))
+        img.save("test.png")
 
 
 if __name__ == "__main__":
     map = Map("elevation_small.txt")
+    map.read_file()
+    map.find_elevations()
+    map.find_min_and_max()
+    map.get_colors_from_elevations()
+    map.create_map_image()
--- practice2.py
+++ practice2.py
@@ -9,15 +9,15 @@ elevations = [[int(each) for each in line.split()]
               for line in text_contents.split("\n")]
 
 
-min = elevations[0][0]
-max = elevations[0][0]
+min_elevation = elevations[0][0]
+max_elevation = elevations[0][0]
 
 for each in elevations:
     for integer in each:
-        if integer < min:
-            min = integer
-        if integer > max:
-            max = integer
+        if integer < min_elevation:
+            min_elevation = integer
+        if integer > max_elevation:
+            max_elevation = integer
 
 
 colors_big_list = []
@@ -25,11 +25,12 @@ little_rows_of_colors = []
 
 for rows in elevations:
     for number in rows:
-        color_int = round(((number - min) / (max-min)) * 255)
+        color_int = round(((number - min_elevation) /
+                           (max_elevation-min_elevation)) * 255)
         little_rows_of_colors.append(color_int)
     colors_big_list.append(little_rows_of_colors)
     little_rows_of_colors = []
 
-img = Image.fromarray(np.uint8(colors_big_list))
+img = Image.fromarray(np.uint8(colors_big_list))≈
 
 img.save('pathfinder.png')
